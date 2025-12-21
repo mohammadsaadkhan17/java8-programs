119b932f04d09a61bc98ba8bb62847f2d124e530
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2025-12-21 18:28:55 +0530

    Add files via upload

---
+++ PartitionEvenOdd.java
@@ -0,0 +1,24 @@
+package streamseg;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class PartitionEvenOdd {
+
+	//Partition numbers in even and odd List
+	public static void main(String[] args) {
+		
+		List<Integer> numbers =Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		Map<Boolean, List<Integer>> evenOdd=numbers.stream()
+				                                   .collect(Collectors.partitioningBy(n -> n%2==0));
+		
+		System.out.println("Even Numbers:"+evenOdd.get(true));
+		System.out.println("Odd Numbers:"+evenOdd.get(false));
+		
+
+	}
+
+}
