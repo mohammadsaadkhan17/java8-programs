b27b60766e9932c461d328638e7a9ac1f4771afb
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-01-08 21:24:53 +0530

    Merge two arrays into single array

---
+++ MergeArray.java
@@ -0,0 +1,19 @@
+package streamseg;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+public class MergeArray {
+
+	public static void main(String[] args) {
+		int[] arr1 = {2,6,5,8,4};
+		int[] arr2 = {3,7,1,9,0};
+		
+		int[] mergeArray =IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2)).sorted().toArray();
+		
+		System.out.println(Arrays.toString(mergeArray));
+
+	}
+
+}
