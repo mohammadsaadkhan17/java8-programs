3f3ed2d83d1e1367317d1a692bb0e710f6902c64
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-01-17 19:04:02 +0530

    List to set

---
+++ ListToSet.java
@@ -0,0 +1,20 @@
+package streamseg;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ListToSet {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list =Arrays.asList(1,2,3,4,5,6,2,4,5);
+		
+		Set<Integer> set =list.stream().collect(Collectors.toSet());
+		
+		System.out.println(set);
+
+	}
+
+}
