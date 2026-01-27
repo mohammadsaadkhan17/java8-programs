a98194e6a8216fcba9aa7765fd4b351a3dedf2cd
Mohammad Saad Khan <126770699+mohammadsaadkhan17@users.noreply.github.com>
2026-01-27 16:59:08 +0530

    group length

--- GroupByLength.java
+++ GroupByLength.java
@@ -10,12 +10,15 @@ public class GroupByLength {
 	//Group a list of words by their length using Streams
 	public static void main(String[] args) {
 		
-		List<String> words=Arrays.asList("apple","orange","mango","grapes","banana");
+		List<String> words=Arrays.asList("apple","bat","ball","cat","banana","dog","goat","A");
 
-		Map<Integer,List<String>> groupBy=words.stream().collect(Collectors.groupingBy(String::length));
 		
-		System.out.println(groupBy);
+		Map<Integer,List<String>> output=words.stream().collect(Collectors.groupingBy(s->s.length()));
+		//OR
+		System.out.println("ONE:"+output);
+		Map<Integer,List<String>> outputTwo=words.stream().collect(Collectors.groupingBy(String::length));
+		
+		System.out.println("TWO:"+outputTwo);
 	}
 
 }
-
